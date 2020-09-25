e1aed40922f8a4629d9e97bcdd4e34fe90d307d4
Ben <bjmorse@gmail.com>
2020-09-25 01:40:03 -0700

    compiles but no dice

--- Makefile
+++ Makefile
@@ -19,7 +19,7 @@ FNAME=main
 
 all: wasm
 
-wasm: out/$(FNAME).wasm
+wasm: out out/$(FNAME).wasm
 
 out:
 	mkdir -p out
@@ -31,10 +31,16 @@ out/side.o: csrc/side.cpp
 out/main.o: csrc/main.cpp
 	em++ csrc/main.cpp -c -o out/main.o $(CLANG_WARN_FLAGS) $(CLANG_OTHER_FLAGS)
 
+# out/$(FNAME).wasm
+# lib/Groot/build/libbehavior_tree_editor.so
 out/$(FNAME).wasm: out out/main.o out/side.o
 	emcc \
 	out/side.o \
 	out/main.o \
+	lib/Groot/build/lib/libbehaviortree_cpp_v3.so \
+	lib/Groot/build/lib/libQtNodeEditor.a \
+	lib/Groot/build/libGroot.a \
+	-s WASM_OBJECT_FILES=0 \
 	-s MAIN_MODULE=1 \
 	-o out/$(FNAME).html \
 	-s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap", "addOnPostRun", "addFunction", "setValue", "getValue"]' \
--- csrc/main.cpp
+++ csrc/main.cpp
@@ -1,9 +1,40 @@
+
+// #include <QCommandLineParser>
+// #include <QApplication>
+// #include <QDialog>
+// #include <nodes/NodeStyle>
+// #include <nodes/FlowViewStyle>
+// #include <nodes/ConnectionStyle>
+// #include <nodes/DataModelRegistry>
+
+// #include "mainwindow.h"
+// #include "XML_utilities.hpp"
+// #include "startup_dialog.h"
+// #include "models/RootNodeModel.hpp"
+
+// using QtNodes::DataModelRegistry;
+// using QtNodes::FlowViewStyle;
+// using QtNodes::NodeStyle;
+// using QtNodes::ConnectionStyle;
+
+// int
+// main(int argc, char *argv[])
+// {
+//     QApplication app(argc, argv);
+//     app.setApplicationName("Groot");
+// }
+
+
 #include <iostream>
 
 using namespace std;
 
 int side(const int a);
 
+void longFunction(void);
+
 int main(void) {
     cout << "Hello world " << side(1) << "\n";
+
+    longFunction();
 }
