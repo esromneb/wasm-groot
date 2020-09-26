8a074d3bf188e37e429fd313bb83710e507ee13b
Ben <bjmorse@gmail.com>
2020-09-26 01:19:05 -0700

    first go at travis build of qmake proj

--- project/main.cpp
+++ project/main.cpp
@@ -1,5 +1,82 @@
 #include <QtGui>
 #include <QtQuick>
+#include <iostream>
+
+using namespace std;
+
+
+
+
+#include "behaviortree_cpp_v3/flatbuffers/bt_flatbuffer_helper.h"
+#include "behaviortree_cpp_v3/bt_factory.h"
+#include <iostream>
+
+using namespace std;
+using namespace BT;
+
+
+
+static const char* xml_text_sequence = R"(
+
+ <root main_tree_to_execute = "MainTree" >
+
+     <BehaviorTree ID="MainTree">
+        <Sequence name="root">
+            <IsDoorOpen/>
+            <IsDoorOpen/>
+        </Sequence>
+     </BehaviorTree>
+
+ </root>
+ )";
+
+
+NodeStatus DummyFunction(void) {
+    return NodeStatus::SUCCESS;
+}
+
+void tryBt(void) {
+    BT::BehaviorTreeFactory factory;
+
+    factory.registerSimpleCondition("IsDoorOpen", std::bind(DummyFunction));
+
+
+    auto tree = factory.createTreeFromText(xml_text_sequence);
+
+    for( const auto& n : tree.nodes ) {
+        cout << n->registrationName() << "->" << n->UID() << "\n";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 int main(int argc, char **argv)
 {
@@ -9,6 +86,9 @@ int main(int argc, char **argv)
     view.setSource(QUrl("qrc:///main.qml"));
     view.setResizeMode(QQuickView::SizeRootObjectToView);
     view.showFullScreen();
+
+    tryBt();
+    cout << "Above exec" << std::endl;
 
     return app.exec();
 }
